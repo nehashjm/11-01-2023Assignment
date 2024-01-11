9c9c7280525c9a794d71d858f484a7b648f6c7c3
Nehash <nehash.j.m027@gmail.com>
2024-01-11 16:04:53 +0530

    day5assignment

---
+++ ConsoleApp1/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? genere { get; set; }
+        public double rating { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+List<Movies> movies = new List<Movies>()
+{
+    new Movies(){Id=1,Title="welcome",genere="comedy",rating=8.3},
+    new Movies(){Id=2,Title="HeraPheri",genere="comedy",rating=8.3},
+    new Movies(){Id=3,Title="Fukrey",genere="comedy",rating=8.3},
+
+};
+Console.WriteLine("name \t genere \t rating");
+foreach (Movies movie in movies)
+{
+    Console.Write(movie.Title + "\t");
+    Console.Write(movie.genere + "\t");
+    Console.Write(movie.rating + "\t");
+    Console.Write("\n");
+}
+Console.WriteLine("enter movie title to search ");
+string name = Console.ReadLine();
+Movies findmovie = GetMovieByName(movies, name);
+
+if (findmovie != null)
+{
+    Console.WriteLine("movie found ");
+}
+else
+{
+    Console.WriteLine("not found");
+}
+static Movies GetMovieByName(List<Movies> movieList, string targetName)
+{
+    return movieList.FirstOrDefault(movie => movie.Title.Equals(targetName, StringComparison.OrdinalIgnoreCase));
+}
+
